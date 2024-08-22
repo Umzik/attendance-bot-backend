6b880b5ba39fe11a0149ff51ca12a9eb42d01e6b
Umzik <70839382+Umzik@users.noreply.github.com>
2024-08-22 13:18:51 +0500

    changes

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
